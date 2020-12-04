f569d4cc9c903109c3f3db9892895c51f79eacf1
lgylgy1919 <59249964+lgylgy1919@users.noreply.github.com>
2020-12-05 00:15:33 +0900

    Room Admin

--- rooms/models.py
+++ rooms/models.py
@@ -77,12 +77,12 @@ class Room(core_models.TimeStampedModel):
     bedrooms = models.IntegerField()
     baths = models.IntegerField()
     check_in = models.TimeField()
-    chekc_out = models.TimeField()
+    check_out = models.TimeField()
     instant_book = models.BooleanField(default=False)
     host = models.ForeignKey("users.User", on_delete=models.CASCADE)
     room_type = models.ForeignKey("RoomType", on_delete=models.SET_NULL, null=True)
     amenities = models.ManyToManyField("Amenity", blank=True)
-    Facilities = models.ManyToManyField("Facility", blank=True)
+    facilities = models.ManyToManyField("Facility", blank=True)
     house_rules = models.ManyToManyField("HouseRule", blank=True)
 
     def __str__(self):
