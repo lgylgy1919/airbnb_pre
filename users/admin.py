dc7b37d35f58de4a9f9e223c56d95ddaa852af90
lgylgy1919 <59249964+lgylgy1919@users.noreply.github.com>
2020-12-15 23:23:50 +0900

    #17 github_login

--- users/admin.py
+++ users/admin.py
@@ -22,6 +22,7 @@ class CustomUserAdmin(UserAdmin):
                     "language",
                     "currency",
                     "superhost",
+                    "login_method",
                 )
             },
         ),
@@ -40,4 +41,7 @@ class CustomUserAdmin(UserAdmin):
         "superhost",
         "is_staff",
         "is_superuser",
+        "email_verified",
+        "email_secret",
+        "login_method",
     )
