379a9d8231abb528c954ff26d0f83fe0d2c26e2d
lgylgy1919 <59249964+lgylgy1919@users.noreply.github.com>
2020-12-24 19:57:08 +0900

    #24 review

--- core/managers.py
+++ core/managers.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 
-class CustomReservationManager(models.Manager):
+class CustomModelManager(models.Manager):
     def get_ro_none(self, **kwargs):
         try:
             return self.get(**kwargs)
